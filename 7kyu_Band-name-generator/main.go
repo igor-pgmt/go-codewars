e182f2bdbb22222100aab4d9f231c74adb39749e
Igor <igor.pgmt@gmail.com>
2018-06-01 22:26:10 +0300

    Added 7 Brand name generator

---
+++ 7kyu_Band-name-generator/main.go
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/59727ff285281a44e3000011
+// Band name generator
+// My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+// "dolphin" -> "The Dolphin"
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+// "alaska" -> "Alaskalaska"
+// Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println(bandNameGenerator("dolphin")) // The Dolphin
+	fmt.Println(bandNameGenerator("alaska"))  // Alaskalaska
+
+}
+
+func bandNameGenerator(word string) string {
+	if word[0] == word[len(word)-1] {
+		return strings.Title(word) + word[1:]
+	}
+	return "The " + strings.Title(word)
+}
